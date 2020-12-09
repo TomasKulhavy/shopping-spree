11c2ef17f24bbfc97759f3dcb862da93707025e9
Tomáš Kulhavý <u17.414@pslib.cloud>
2020-12-09 21:09:48 +0100

    Debug

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { ListGroupItem, Button } from "reactstrap";
 
-<<<<<<< HEAD
 const Item = ({item, removeItem, index, checkItem, textColor}) => (
     <ListGroupItem color={textColor}>
         {item}
         <Button onClick={() => removeItem(index)} color="danger">Remove</Button>
         <Button onClick={() => checkItem(index)} color="success">Check</Button>
-=======
-const Item = ({item, removeItem, index}) => (
-    <ListGroupItem>
-        {item}
-        <Button onClick={() => removeItem(index)} color="danger">Remove</Button>
->>>>>>> d4ebeb0160a51bf168e909abc56e74a8e2564c38
     </ListGroupItem>
 )
 
